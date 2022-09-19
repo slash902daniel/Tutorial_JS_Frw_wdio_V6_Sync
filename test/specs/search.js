079e80e836174d3887392b9b216d2348a4f8f2ca
leosanchez <slash902daniel@gmail.com>
2022-09-19 14:47:28 -0500

    Begin|lsanchez|Tutorial3

--- test/specs/search.js
+++ test/specs/search.js
@@ -1,7 +1,29 @@
-// describe('Search Description', () => {
-//     it('Should have title', () => {
-//         browser.url('https://google.com')
-//         expect(browser).toHaveTitle('some')
-        
-//     })
-// })
+describe('Search Description', () => {
+    it('Should have title', () => {
+        browser.url('https://google.com');
+        expect(browser).toHaveTitle('Google');      
+    })
+
+    it('Should Do Search and verify text value', () => {
+        let searchValue = 'Amazon mexico';
+        let searchInput = $('input[aria-label="Buscar"]');
+        let searchButton = $$('input[name="btnK"]')[1];
+
+        searchInput.addValue(searchValue);
+        searchButton.click();
+
+        expect(searchInput).toHaveValue(searchValue);
+    })
+
+    it('Should Redirect to new page and verify Title', () => {
+        expect(browser).toHaveTitle('Amazon mexico - Buscar con Google');
+    })
+
+    // it('Should have a counter of matches', () => {
+    //     let resultsStats = $('div#result-stats');
+    //     console.log('resultsStats: ' + resultsStats.getText);
+    //     console.log('resultsStats2: ' + resultsStats.getValue);
+    //     expect(resultsStats).toHaveValueContaining('Cerca de');
+    //     expect(resultsStats).toHaveValueContaining('resultados');
+    // })
+})
